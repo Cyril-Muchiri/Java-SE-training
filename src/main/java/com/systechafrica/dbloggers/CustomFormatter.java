cff1dc20eff89074fe2e3cd0b421dc7aa54fe819
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-09-26 09:37:59 +0300

    new log file

--- src/main/java/com/systechafrica/dbloggers/CustomFormatter.java
+++ src/main/java/com/systechafrica/dbloggers/CustomFormatter.java
@@ -4,23 +4,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
-import java.util.regex.Pattern;
-
-import javax.swing.text.DateFormatter;
 
 public class CustomFormatter extends Formatter {
-
     @Override
     public String format(LogRecord record) {
-       
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-       String classMethod= record.getSourceClassName()+" "+record.getSourceMethodName();
-       String level=record.getLevel().getName();
-String message= record.getMessage();
-LocalDateTime now=LocalDateTime.now();
 
-return pattern.format(now)+"|"+level+"|"+classMethod+"|"+message+"\n";
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String classMethod = record.getSourceClassName() + " " + record.getSourceMethodName();
+        String level = record.getLevel().getName();
+        String message = record.getMessage();
+        LocalDateTime now = LocalDateTime.now();
+
+        return pattern.format(now) + " | " + level + " | " + classMethod + " | " + message + "\n";
         // throw new UnsupportedOperationException("Unimplemented method 'format'");
     }
-    
+
 }
