ed5a74d25f2fab92f48bae3379202a4a392149cd
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-08 12:57:15 +0300

    refactored my login helper class

--- src/main/java/com/systechafrica/loginhelper/LoginHelper.java
+++ src/main/java/com/systechafrica/loginhelper/LoginHelper.java
@@ -1,25 +1,22 @@
 package com.systechafrica.loginhelper;
-import java.sql.Connection;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.systechafrica.dbconfig.DbConnector;
 
 public class LoginHelper {
-    java.sql.Statement statement;
     java.sql.CallableStatement callableStatement;
     DbConnector connector = DbConnector.createConnector();
-    Connection connection = connector.getConnection();
     ResultSet resultSet;
     boolean isOwner;
 
     public boolean checkUserCredentials(String password) {
         try {
-            statement = connection.createStatement();
-            callableStatement = connection.prepareCall("{CALL selectMember(?)}");
-           callableStatement.setString(1,password);
+            callableStatement = connector.getConnection().prepareCall("{CALL selectMember(?)}");
+            callableStatement.setString(1, password);
             resultSet = callableStatement.executeQuery();
-        
+
             while (resultSet.next()) {
                 String storedPassword = resultSet.getString(2);
 
