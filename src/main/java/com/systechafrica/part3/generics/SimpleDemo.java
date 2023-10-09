1c44d50fde0c286702770f84b23977c5a1270d85
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-09 13:34:17 +0300

    added a generic class

--- src/main/java/com/systechafrica/part3/generics/SimpleDemo.java
+++ src/main/java/com/systechafrica/part3/generics/SimpleDemo.java
@@ -10,14 +10,33 @@ public class SimpleDemo {
         displayArray(numbers);
         displayArray(names);
         displayArray(alphabet);
+System.out.println("");
+        GenericStorage store1=new GenericStorage<>();
 
+        Grocery maize=new Grocery();
+        maize.setGroceryQuantity(200);
+        Grocery beans= new Grocery();
+        beans.setGroceryQuantity(100);
+
+        store1.store(beans,beans.getGroceryQuantity());
+        store1.store(maize,maize.getGroceryQuantity());
+
+
+
+    }
+
+   
+
+    <SomeGroceries> void displayGrocery(Grocery[] item) {
+        for (Grocery someGrocery : item) {
+            System.out.println(someGrocery);
+        }
     }
 
     static <SomeArray> void displayArray(SomeArray[] array) {
         for (SomeArray someArray : array) {
-         
-            System.out.print(someArray+", ");
-            
+            System.out.print(someArray + ", ");
+
         }
     }
 }
