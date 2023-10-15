e58ef4310196e64ef049550078bf7549c25455c4
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-15 22:36:37 +0300

    added a few methods on the set collections

--- src/main/java/com/systechafrica/part3/collections/SetsDemo.java
+++ src/main/java/com/systechafrica/part3/collections/SetsDemo.java
@@ -4,17 +4,39 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetsDemo {
-    
+
     public static void main(String[] args) {
-        
-        Set<Object>setOfNames= new HashSet<>();
 
-        StringBuilder sergernt=new StringBuilder("Kimaiyu");
+        Set<Object> setOfNames = new HashSet<>();
 
+        StringBuilder sergernt = new StringBuilder("Kimaiyu");
+        // ?sets do not allow duplicates
         setOfNames.add("Kimani");
         setOfNames.add(sergernt);
+        setOfNames.add(sergernt);
+        setOfNames.add(100);
+        setOfNames.add("Constable");
 
+        setOfNames.remove("Kimani");
+        int length = setOfNames.size();
+        // setOfNames.clear();//clears all elements in the set
+        setOfNames.contains(sergernt);
+
+        for (Object object : setOfNames) {
+            System.out.println(object);
+        }
+        // using a lambda
+        System.out.println("This is by using a for each method and lambda");
+        setOfNames.forEach(System.out::println);
+
+        // using an interator interface
+        System.out.println("This is by using an iterator");
+        java.util.Iterator<Object> objIterator = setOfNames.iterator();
+        while (objIterator.hasNext()) {
+            System.out.println(objIterator.next());
+        }
 
         System.out.println(setOfNames);
+        System.out.println(length);
     }
 }
