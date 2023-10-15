4b080c1593b09a5e981688ce7e90d16560ecf760
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-16 00:14:39 +0300

    added a linked list

---
+++ src/main/java/com/systechafrica/part3/collections/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.systechafrica.part3.collections;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Object> list=new LinkedList<>();
+
+        list.add(100);
+        list.add("Cat");
+        list.add(90.893);
+        list.add('C');
+
+        //*Can accept duplicates */
+        //*follow insertion order */
+
+    }
+}
