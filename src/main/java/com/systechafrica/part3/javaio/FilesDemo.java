500cd407b318b18db46720312920d3fdb7f9f90c
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-16 20:50:59 +0300

    added a bufferedreader

---
+++ src/main/java/com/systechafrica/part3/javaio/FilesDemo.java
@@ -0,0 +1,34 @@
+package com.systechafrica.part3.javaio;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilesDemo {
+    public static void main(String[] args) {
+        fileStreams();
+
+    }
+
+    private static void fileStreams() {
+        String fileSeparator = System.getProperty("file.separator");
+        File sample = new File("sample.txt");// file separotor ie /
+        if (sample.exists() && sample.isFile()) {
+            System.out.println("exists!!");
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(sample));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("does not exist!");
+        }
+    }
+}
