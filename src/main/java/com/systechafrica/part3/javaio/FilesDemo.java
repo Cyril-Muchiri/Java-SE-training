dd096b7fbc124c664ea6c13438a93115aea935a0
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-18 15:28:26 +0300

    added a buffered wrtiter in append mode

--- src/main/java/com/systechafrica/part3/javaio/FilesDemo.java
+++ src/main/java/com/systechafrica/part3/javaio/FilesDemo.java
@@ -1,27 +1,32 @@
 package com.systechafrica.part3.javaio;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class FilesDemo {
     public static void main(String[] args) {
         // fileStreams();
-        FilesDemo demmo= new FilesDemo();
-        try  {
-            demmo.fileIo();
+        FilesDemo demo = new FilesDemo();
+        try {
+            demo.writeToFile();
+            Thread.sleep(3000);
+            demo.fileIo();
         } catch (Exception e) {
-          e.getMessage();
+            e.getMessage();
         }
-        
 
     }
 
     private static void fileStreams() {
         String fileSeparator = System.getProperty("file.separator");
-        File sample = new File("sample.txt");// file separotor ie /
+        File sample = new File("sampleA.txt");// file separotor ie /
         if (sample.exists() && sample.isFile()) {
             System.out.println("exists!!");
             try {
@@ -43,22 +48,34 @@ public class FilesDemo {
 
     private void fileIo() throws IOException {
         File file = new File("sampleA.txt");
-        
+
         if (file.exists()) {
             System.out.println("This file exists!!");
         } else {
             System.out.println("file does not exist");
             file.createNewFile();
         }
-
         FileReader reader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(reader);
-
         String line;
-
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
         }
+    }
 
+    private void writeToFile() {
+        try {
+            // fileExists=(file.exists())?true:false;
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("sampleA.txt",true));
+            writer.write("\nCow is mooing");
+            writer.close();
+        }
+
+        catch (
+
+        IOException e) {
+            e.printStackTrace();
+        }
     }
 }
