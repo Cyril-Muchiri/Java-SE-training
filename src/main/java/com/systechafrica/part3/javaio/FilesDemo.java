ab30186d3c8f1a7a80d11da541419a9f94b64599
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-10-18 09:48:08 +0300

    created a new file

--- src/main/java/com/systechafrica/part3/javaio/FilesDemo.java
+++ src/main/java/com/systechafrica/part3/javaio/FilesDemo.java
@@ -2,12 +2,20 @@ package com.systechafrica.part3.javaio;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FilesDemo {
     public static void main(String[] args) {
-        fileStreams();
+        // fileStreams();
+        FilesDemo demmo= new FilesDemo();
+        try  {
+            demmo.fileIo();
+        } catch (Exception e) {
+          e.getMessage();
+        }
+        
 
     }
 
@@ -31,5 +39,26 @@ public class FilesDemo {
         } else {
             System.out.println("does not exist!");
         }
+    }
+
+    private void fileIo() throws IOException {
+        File file = new File("sampleA.txt");
+        
+        if (file.exists()) {
+            System.out.println("This file exists!!");
+        } else {
+            System.out.println("file does not exist");
+            file.createNewFile();
+        }
+
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+
     }
 }
